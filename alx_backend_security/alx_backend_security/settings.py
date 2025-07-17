28b668b0a35442bdf7618343db32fd40865f16b3
Muliro1 <mulirokhaemba@gmail.com>
2025-07-17 20:56:25 +0300

    ip

--- alx_backend_security/alx_backend_security/settings.py
+++ alx_backend_security/alx_backend_security/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ip_tracking',
+    'ratelimit',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-ratelimit settings
+RATELIMIT_USE_CACHE = 'default'
+RATELIMIT_ENABLE = True
--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from ratelimit.decorators import ratelimit
 
 # Create your views here.
+
+# Sensitive login view with rate limiting
+@ratelimit(key='ip', rate='10/m', method='POST', block=True)
+@ratelimit(key='ip', rate='5/m', method='POST', block=True, group='anon', condition=lambda r: not r.user.is_authenticated)
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Login successful')
+        else:
+            return HttpResponse('Invalid credentials', status=401)
+    return render(request, 'login.html')
